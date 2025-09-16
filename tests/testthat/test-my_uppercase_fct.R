eb16b622f6f8c954c90ae2c2a053c34ae8ea54bc
FrederikkeOmicron <frederikke.modin@omicron.dk>
2025-09-16 15:46:42 +0200

    change test file

---
+++ tests/testthat/test-my_uppercase_fct.R
@@ -0,0 +1,3 @@
+test_that("uppercase works",{
+  expect_equal(my_uppercase_fct("hello world"), "HELLO WORLD")
+})
--- tests/testthat/test-return_uppercase.R
+++
@@ -1,3 +0,0 @@
-test_that("uppercase works",{
-  expect_equal(return_uppercase("hello world"), "HELLO WORLD")
-})
