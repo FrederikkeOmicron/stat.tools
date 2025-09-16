4f794672fcf9820015b2bd21816d56bf32aae2fe
FrederikkeOmicron <frederikke.modin@omicron.dk>
2025-09-16 15:38:19 +0200

    inserted test for return_uppercase

--- R/toy_function.R
+++ R/toy_function.R
@@ -1,4 +1,13 @@
-x <- "hello world"
+#' Title
+#'
+#' @param x A character string
+#'
+#' @returns The character string in uppercase
+#' @export
+#'
+#' @examples
+#' x <- "hello world"
+#' return_uppercase(x)
 
 return_uppercase <- function(x){
 
---
+++ tests/testthat/test-return_uppercase.R
@@ -0,0 +1,3 @@
+test_that("uppercase works",{
+  expect_equal(return_uppercase("hello world"), "HELLO WORLD")
+})
